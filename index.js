a86bb35e4d5e02671a05711a4797d3b18b4691d4
Eyup3 <eyup.ozturk@outlook.de>
2021-03-28 00:00:59 +0000

    Add semicolons :DX

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const app = express();
 const spawn = require('child_process').spawn;
-const fs = require('fs')
+const fs = require('fs');
 
 console.log('Parse Config');
-let config = JSON.parse((fs.readFileSync("./config.json")))
+let config = JSON.parse((fs.readFileSync("./config.json")));
 
-console.log("Get edited Html")
+console.log("Get edited Html");
 let editetHtml = require('./htmlEditor').HTML;
 
 app.get('/', (req, res) => {
-    res.send(editetHtml)
-    console.log("Served Website")
-})
+    res.send(editetHtml);
+    console.log("Served Website");
+});
 
 app.post('/:cmd', (req, res) => {
-    console.log("Get command " + req.params.cmd)
-    callScript(req.params.cmd)
-})
+    console.log("Get command " + req.params.cmd);
+    callScript(req.params.cmd);
+});
 
 app.listen(config.port, () => { console.log("Server is listening") });
 
@@ -29,11 +29,11 @@ const callScript = (command) => {
         }).on('message', (data) => {
             console.log(data);
         });
-console.log("Spawned " + path)
-        setTimeout(() => {
-            proc.kill()
-        }, JSON.parse((fs.readFileSync(path + "/" + "params.json")).maxlength || 5) * 1000, 0)
+	console.log("Spawned " + path);
 
-    } catch (e) { console.log(e) }
+        setTimeout(
+	() => { proc.kill() }, 
+	JSON.parse((fs.readFileSync(path + "\\" + "params.json")).maxlength || 5) * 1000, 0);
+    } catch (e) { console.log(e); }
 
-}
+};
